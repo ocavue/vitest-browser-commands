3433ad0f2355675accf7f69a1b8cd6095c9108a3
ocavue <ocavue@gmail.com>
2025-10-25 23:33:04 +1100

    add all

--- src/playwright/node.ts
+++ src/playwright/node.ts
@@ -1,7 +1,11 @@
 import type { Plugin } from 'vitest/config'
 
+import { playwrightMouseClick } from './commands/mouse-click'
+import { playwrightMouseDblclick } from './commands/mouse-dblclick'
 import { playwrightMouseDown } from './commands/mouse-down'
+import { playwrightMouseMove } from './commands/mouse-move'
 import { playwrightMouseUp } from './commands/mouse-up'
+import { playwrightMouseWheel } from './commands/mouse-wheel'
 
 /**
  * Returns a vite plugin that adds some useful playwright commands to the vitest browser commands context
@@ -14,8 +18,12 @@ export function playwrightCommands(): Plugin {
         test: {
           browser: {
             commands: {
+              playwrightMouseClick,
+              playwrightMouseDblclick,
               playwrightMouseDown,
+              playwrightMouseMove,
               playwrightMouseUp,
+              playwrightMouseWheel,
             },
           },
         },
