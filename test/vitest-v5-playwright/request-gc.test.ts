691e1beac4e82c6c7c4db995d4fd72044fda93e0
ocavue <ocavue@gmail.com>
2026-08-31 02:46:01 +1200

    fix

--- test/vitest-v5-playwright/request-gc.test.ts
+++ test/vitest-v5-playwright/request-gc.test.ts
@@ -3,18 +3,36 @@ import { requestGC } from 'vitest-browser-commands/playwright'
 
 describe('requestGC', () => {
   it('should collect an unreachable object', async () => {
-    const global = globalThis as typeof globalThis & {
-      suspect?: { hello: string } | null
-      suspectWeakRef?: WeakRef<{ hello: string }>
-    }
-    global.suspect = { hello: 'world' }
-    global.suspectWeakRef = new WeakRef(global.suspect)
+    const fixture = createSuspect()
+    expect(fixture.hasSuspect()).toBe(true)
 
     await requestGC()
-    expect(global.suspectWeakRef.deref()).toEqual({ hello: 'world' })
+    expect(fixture.hasSuspect()).toBe(true)
 
-    global.suspect = null
+    fixture.reset1()
     await requestGC()
-    expect(global.suspectWeakRef.deref()).toBeUndefined()
+    expect(fixture.hasSuspect()).toBe(true)
+
+    fixture.reset2()
+    await requestGC()
+    expect(fixture.hasSuspect()).toBe(false)
   })
 })
+
+function createSuspect() {
+  const global = globalThis as typeof globalThis & {
+    suspect1?: { hello: string } | null
+    suspect2?: { hello: string } | null
+    suspectWeakRef?: WeakRef<{ hello: string }>
+  }
+  const suspect = { hello: 'world' }
+  global.suspect1 = suspect
+  global.suspect2 = suspect
+  global.suspectWeakRef = new WeakRef(global.suspect1)
+
+  return {
+    hasSuspect: () => !!global.suspectWeakRef?.deref(),
+    reset1: () => (global.suspect1 = null),
+    reset2: () => (global.suspect2 = null),
+  }
+}
